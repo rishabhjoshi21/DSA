7689373d91c464f679a2135bb4faeaa4c8e2c0b4
rishabh joshi <51262109+rishabhjoshi21@users.noreply.github.com>
2026-08-07 00:40:05 +0530

    [1] [Time Beats: N/A] [Memory Beats: N/A] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/1. Two Sum/1. Two Sum.js
@@ -0,0 +1,1 @@
+/** * @param {number[]} nums * @param {number} target * @return {number[]} */var twoSum = function(nums, target) {    const map = new Map()    for (let i = 0; i < nums.length; i++) {        const complement = target - nums[i];        if (map.has(complement)) {
