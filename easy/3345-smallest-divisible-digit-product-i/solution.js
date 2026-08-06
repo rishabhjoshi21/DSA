495c8703bc12214c5bcb58db465382cbcf6bd1f8
rishabh joshi <51262109+rishabhjoshi21@users.noreply.github.com>
2026-08-07 00:05:26 +0530

    ✅ Smallest Divisible Digit Product I | Easy | JavaScript | Runtime: 0 ms | Memory: 53.4 MB | 2026-08-06

--- easy/3345-smallest-divisible-digit-product-i/solution.js
+++ easy/3345-smallest-divisible-digit-product-i/solution.js
@@ -1,9 +1,22 @@
 /**
- * @param {number} n
- * @param {number} t
- * @return {number}
- */
-var smallestNumber = function(n, t) {
-    
-    console.log(typeof n.toString())
+ * @param {number} n
+ * @param {number} t
+ * @return {number}
+ */
+var smallestNumber = function(n, t) {
+    const num = n.toString();
+    const multi = num[0] * num[1];
+    if(n<10) {
+        if(n%t === 0){
+            return n
+        }else{
+            return smallestNumber(n+1,t)
+        }
+    }
+    if(multi%t === 0){
+        return n
+    }else{
+        return smallestNumber(n+1,t)
+    }
+    
 };
