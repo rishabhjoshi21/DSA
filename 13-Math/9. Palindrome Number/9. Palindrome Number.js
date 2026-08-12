a96419e20dbdc613744aea06cd371032b3073b18
rishabh joshi <51262109+rishabhjoshi21@users.noreply.github.com>
2026-08-12 23:54:43 +0530

    [9] [Time Beats: 45.31%] [Memory Beats: 83.96%] - Leet2Hub

--- 13-Math/9. Palindrome Number/9. Palindrome Number.js
+++ 13-Math/9. Palindrome Number/9. Palindrome Number.js
@@ -1,1 +1,15 @@
-/** * @param {number} x * @return {boolean} */var isPalindrome = function(x) {    let reverse = 0;    let copy = x;    while(copy>0){        const digit = copy%10;
+1/**
+2 * @param {number} x
+3 * @return {boolean}
+4 */
+5var isPalindrome = function(x) {
+6    let reverse = 0;
+7    let copy = x;
+8
+9    while(copy>0){
+10        const digit = copy%10;
+11        reverse = reverse *10 + digit
+12        copy = ~~(copy / 10)
+13    }
+14    return reverse === x
+15};
